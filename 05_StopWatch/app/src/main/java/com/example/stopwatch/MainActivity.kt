2804e701927e1f09cd048f61aef55dbe8a697310
MinSoo <minsu_510@naver.com>
2024-09-24 21:25:56 +0900

    메인 액티비티 구현하기 (1) - 골격 생성

--- 05_StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ 05_StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.stopwatch
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+
+    private lateinit var btn_start : Button
+    private lateinit var btn_restart : Button
+    private lateinit var tv_minute : TextView
+    private lateinit var tv_second : TextView
+    private lateinit var tv_millisecond : TextView
+
+    private val isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btn_start = findViewById(R.id.button_start)
+        btn_restart = findViewById(R.id.button_restart)
+        tv_minute = findViewById(R.id.tv_minute)
+        tv_second = findViewById(R.id.tv_second)
+        tv_millisecond = findViewById(R.id.tv_millisecond)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.button_start -> {
+                if (isRunning) {
+                    pause()
+                } else {
+                    start()
+                }
+            }
+
+            R.id.button_restart -> {
+                restart()
+            }
+        }
+    }
+
+    private fun start() {
+
+    }
+
+    private fun pause() {
+
+    }
+
+    private fun restart() {
+
     }
 }
