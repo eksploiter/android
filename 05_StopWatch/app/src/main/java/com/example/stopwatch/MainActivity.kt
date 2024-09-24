d56518e756cc7929289359b74622068594f36724
MinSoo <minsu_510@naver.com>
2024-09-24 21:42:33 +0900

    메인 액티비티 구현하기 (2) - Start() 함수 구현하기 (1)

--- 05_StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ 05_StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
 
@@ -17,7 +19,10 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     private lateinit var tv_second : TextView
     private lateinit var tv_millisecond : TextView
 
-    private val isRunning = false
+    private var isRunning = false
+
+    private var timer : Timer? = null
+    private var time = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +58,24 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     }
 
     private fun start() {
+        btn_start.text = getString(R.string.btn_pause)
+        btn_start.setBackgroundColor(getColor(R.color.btn_pause))
+        isRunning = true
 
+        timer = timer(period = 10) {
+            // 1000ms = 1s
+            // 0.01 time 1+
+            time++
+
+            // 3개의 텍스트 안에 값을 어떻게 넣어 줄지
+            val milli_second = time % 100
+            val second = (time % 6000) / 100
+            val minute = time / 6000
+
+            tv_millisecond.text = if(milli_second < 10) ".0${milli_second}" else ".${milli_second}"
+            tv_second.text = if (second < 10) ":0${second}" else ":${second}"
+            tv_minute.text = "${minute}"
+        }
     }
 
     private fun pause() {
